74d1b6e3b4a0a84a51dd20b80d93e838eeb93d8d
steven <nguyenthanhvo06031999@gmail.com>
2022-11-29 13:17:31 +0700

    CRUD contact

---
+++ server/index.js
@@ -0,0 +1,71 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mysql from 'mysql2';
+
+const db = mysql.createPool({
+	host: "127.0.0.1",
+	user: "root",
+	password: "Steven@123",
+	database: "crud_contact",
+	port: 3306,
+});
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/api/get", (req, res) => {
+	const sqlGet = "SELECT * FROM contact_db";
+	db.query(sqlGet, (error, result) => {
+		res.send(result);
+	});
+});
+
+app.post("/api/post", (req, res) => {
+	const { name, email, contact } = req.body;
+	const sqlInsert = "INSERT INTO contact_db (name, email, contact) VALUES (?,?,?)";
+	db.query(sqlInsert, [name, email, contact], (error, result) => {
+		if(error) {
+			console.log(error)
+		}
+	})
+})
+
+app.delete("/api/remove/:id", (req, res) => {
+	const { id } = req.params;
+	const sqlRemove = "DELETE FROM contact_db Where id = ?";
+	db.query(sqlRemove, id, (error, result) => {
+		if(error) {
+			console.log(error)
+		}
+	})
+});
+
+app.get("/api/get/:id", (req, res) => {
+	const { id } = req.params;
+	const sqlGet = "SELECT * FROM contact_db where id = ?";
+	db.query(sqlGet, id,(error, result) => {
+		if(error) {
+			console.log(error);
+		}
+		res.send(result);
+	});
+});
+
+app.put("/api/update/:id", (req, res) => {
+	const { id } = req.params;
+	const { name, email, contact } = req.body;
+	const sqlUpdate = "UPDATE contact_db SET name = ?, email = ?, contact = ? WHERE id = ?";
+	db.query(sqlUpdate, [name, email, contact, id],(error, result) => {
+		if(error) {
+			console.log(error);
+		}
+		res.send(result);
+	});
+});
+
+app.listen(8080, () => {
+	console.log('Server run port 8080')
+})
